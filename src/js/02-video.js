b1cf203be7ab5b423f8ffde2881c1f8680ba3bf1
he1gi <oleh.mishchenko1@gmail.com>
2022-08-09 16:25:32 +0300

    added two tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const setCurrentTime = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+player.on('timeupdate', setCurrentTime);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+
+const STORAGE_KEY = 'feedback-msg';
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', a);
+refs.input.addEventListener('input', a);
+
+function onTextInput(event) {
+  const message = event.target.value;
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.target.reset();
+}
+
+function a() {
+  const savedMassage = localStorage.getItem(STORAGE_KEY);
+  if (savedMassage) {
+    refs.input.value = savedMassage;
+  }
+  if (savedMassage) {
+    refs.textarea.value = savedMassage;
+  }
+}
