3fd3f9a5baf5d6aa74370bba0c2ea1c9cb880f87
he1gi <oleh.mishchenko1@gmail.com>
2022-08-09 17:13:21 +0300

    added changes in task 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,15 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const setCurrentTime = throttle(data => {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
-}, 1000);
+function setCurrentTime(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}
 
-player.on('timeupdate', setCurrentTime);
+player.on('timeupdate', throttle(setCurrentTime, 1000));
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
