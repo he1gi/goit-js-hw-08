93f83e0fca91de7c7d47193132d83dce83f4cfa0
he1gi <oleh.mishchenko1@gmail.com>
2022-08-10 18:25:36 +0300

    bugfix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,12 +6,33 @@ const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const setCurrentTime = throttle(data => {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
-}, 1000);
+// const setCurrentTime = throttle(data => {
+//   localStorage.setItem('videoplayer-current-time', data.seconds);
+// }, 1000);
 
-player.on('timeupdate', data => {
-  setCurrentTime(data);
-});
+// player.on('timeupdate', data => {
+//   setCurrentTime(data);
+// });
 
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+// player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+  }, 1000)
+);
+
+const actualTime = localStorage.getItem(STORAGE_KEY);
+
+player
+  .setCurrentTime(actualTime)
+  .then(function () {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
